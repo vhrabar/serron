b31840b79a90170f5d9fc9773a4c1b868158912a
Vedran Hrabar <vedran.hrabar@outlook.com>
2026-07-09 21:26:29 +0200

    feat(format): reformat c++ code to spec #2
    Signed-off-by: Vedran Hrabar <vedran.hrabar@outlook.com>

--- packages/serron/csrc/ops.h
+++ packages/serron/csrc/ops.h
@@ -1,14 +1,13 @@
 #ifndef SERRON_OPS_H
 #define SERRON_OPS_H
 
-
 #include <torch/torch.h>
 
 namespace serron {
 
-	at::Tensor erode(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
-	at::Tensor dilate(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
+at::Tensor erode(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
+at::Tensor dilate(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
 
-}  // namespace serron
+} // namespace serron
 
-#endif  // SERRON_OPS_H
+#endif // SERRON_OPS_H
