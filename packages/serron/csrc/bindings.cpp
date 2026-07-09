f4aa18f359cdf8bd85a49c7cafa3475e51ee8714
Vedran Hrabar <vedran.hrabar@outlook.com>
2026-07-09 18:44:34 +0200

    Add bindings and ops/lib registartion #2
    Signed-off-by: Vedran Hrabar <vedran.hrabar@outlook.com>

---
+++ packages/serron/csrc/bindings.cpp
@@ -0,0 +1,18 @@
+#include <torch/library.h>
+
+#include "ops.h"
+#include "registration.h"
+
+// Operator schemas, registered under the serron:: namespace.
+TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
+  ops.def("erode(Tensor input, Tensor kernel, int border) -> Tensor");
+  ops.def("dilate(Tensor input, Tensor kernel, int border) -> Tensor");
+}
+
+// CUDA implementations.
+TORCH_LIBRARY_IMPL_EXPAND(TORCH_EXTENSION_NAME, CUDA, ops) {
+  ops.impl("erode", &serron::erode);
+  ops.impl("dilate", &serron::dilate);
+}
+
+REGISTER_EXTENSION(TORCH_EXTENSION_NAME)
---
+++ packages/serron/csrc/ops.h
@@ -0,0 +1,14 @@
+#ifndef SERRON_OPS_H
+#define SERRON_OPS_H
+
+
+#include <torch/torch.h>
+
+namespace serron {
+
+	at::Tensor erode(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
+	at::Tensor dilate(const at::Tensor& input, const at::Tensor& kernel, int64_t border);
+
+}  // namespace serron
+
+#endif  // SERRON_OPS_H
